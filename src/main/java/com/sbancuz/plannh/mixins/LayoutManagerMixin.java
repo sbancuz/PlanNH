9d4cdd58e14d9fb6f153f79af4b9315b9b6ba617
sbancuz <sbancuz@protonmail.com>
2026-06-21 17:33:13 +0200

    cleanup

--- src/main/java/com/sbancuz/plannh/mixins/LayoutManagerMixin.java
+++ src/main/java/com/sbancuz/plannh/mixins/LayoutManagerMixin.java
@@ -3,6 +3,7 @@ package com.sbancuz.plannh.mixins;
 import net.minecraft.client.gui.inventory.GuiContainer;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,10 +17,14 @@ import codechicken.nei.VisiblityData;
 @Mixin(value = LayoutManager.class, remap = false)
 public class LayoutManagerMixin {
 
-    private static final Button openFlowchartButton = new OpenFlowchartButton();
+    @Unique
+    private static final Button planNH$openFlowchartButton = new OpenFlowchartButton();
 
+    @Unique
     private static final int FC_BUTTON_GAP = 2;
+    @Unique
     private static final int FC_BUTTON_BOTTOM_OFF = 22;
+    @Unique
     private static final int FC_BUTTON_SIZE = 20;
 
     @Inject(method = "updateWidgetVisiblities", at = @At("TAIL"), remap = false)
@@ -27,10 +32,10 @@ public class LayoutManagerMixin {
         final CallbackInfo ci) {
         if (!visiblity.showNEI) return;
 
-        openFlowchartButton.x = LayoutManager.bookmarksButton.x + LayoutManager.bookmarksButton.w + FC_BUTTON_GAP;
-        openFlowchartButton.y = gui.height - FC_BUTTON_BOTTOM_OFF;
-        openFlowchartButton.h = FC_BUTTON_SIZE;
-        openFlowchartButton.w = FC_BUTTON_SIZE;
-        LayoutManager.addWidget(openFlowchartButton);
+        planNH$openFlowchartButton.x = LayoutManager.bookmarksButton.x + LayoutManager.bookmarksButton.w + FC_BUTTON_GAP;
+        planNH$openFlowchartButton.y = gui.height - FC_BUTTON_BOTTOM_OFF;
+        planNH$openFlowchartButton.h = FC_BUTTON_SIZE;
+        planNH$openFlowchartButton.w = FC_BUTTON_SIZE;
+        LayoutManager.addWidget(planNH$openFlowchartButton);
     }
 }
