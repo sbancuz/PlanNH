08383cd2e685fb772f36ad842909f42126e1a23d
TheYoingLad <golyhagergo@gmail.com>
2026-07-02 10:29:26 +0200

    fixed draggable offset

--- src/main/java/com/sbancuz/plannh/gui/FlowchartTextFieldWidget.java
+++ src/main/java/com/sbancuz/plannh/gui/FlowchartTextFieldWidget.java
@@ -2,12 +2,8 @@ package com.sbancuz.plannh.gui;
 
 import org.jetbrains.annotations.NotNull;
 
-import com.cleanroommc.modularui.api.widget.IWidget;
 import com.cleanroommc.modularui.screen.viewport.ModularGuiContext;
-import com.cleanroommc.modularui.widget.ParentWidget;
 import com.cleanroommc.modularui.widgets.textfield.BaseTextFieldWidget;
-
-import it.unimi.dsi.fastutil.Pair;
 
 public abstract class FlowchartTextFieldWidget extends BaseTextFieldWidget<FlowchartTextFieldWidget>
     implements IFlowchartDraggable {
@@ -47,18 +43,5 @@ public abstract class FlowchartTextFieldWidget extends BaseTextFieldWidget<Flowc
     @Override
     public FlowchartWidget<?, ?> getFlowchartParent() {
         return parent;
-    }
-
-    @Override
-    public Pair<Integer, Integer> dragOffset() {
-        int x = getArea().rx;
-        int y = getArea().ry;
-        IWidget parentWidget = getParent();
-        while (parentWidget instanceof ParentWidget<?>parentWidgetActual && parentWidget != parent) {
-            x += parentWidget.getArea().rx;
-            y += parentWidget.getArea().ry;
-            parentWidget = parentWidgetActual.getParent();
-        }
-        return Pair.of(x, y);
     }
 }
