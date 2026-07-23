510ebdfcffa443a6dcee47c7217e4c7cdfb4e3af
TheYoingLad <golyhagergo@gmail.com>
2026-07-23 11:10:40 +0200

    cycle button changes

--- src/main/java/com/sbancuz/plannh/gui/FlowchartScreen.java
+++ src/main/java/com/sbancuz/plannh/gui/FlowchartScreen.java
@@ -25,7 +25,7 @@ import com.cleanroommc.modularui.value.StringValue;
 import com.cleanroommc.modularui.widget.Widget;
 import com.cleanroommc.modularui.widget.sizer.Area;
 import com.cleanroommc.modularui.widgets.ButtonWidget;
-import com.cleanroommc.modularui.widgets.EnumCycleButtonWidget;
+import com.cleanroommc.modularui.widgets.CycleButtonWidget;
 import com.cleanroommc.modularui.widgets.ListWidget;
 import com.cleanroommc.modularui.widgets.ToggleButton;
 import com.cleanroommc.modularui.widgets.layout.Flow;
@@ -171,7 +171,7 @@ public class FlowchartScreen extends ModularScreen {
                                 .overlay(IKey.str("S2G"))
                                 .addTooltipLine("Snap to Grid"))
                         .child(
-                            new EnumCycleButtonWidget<>(BalanceMode.class)
+                            new CycleButtonWidget()
                                 .value(
                                     new EnumValue.Dynamic<>(
                                         BalanceMode.class,
@@ -179,14 +179,12 @@ public class FlowchartScreen extends ModularScreen {
                                             .getBalanceMode(),
                                         val -> Plan.getActiveGraph()
                                             .setBalanceMode(val)))
-                                .overlay(val -> IKey.str("M:" + switch (val) {
-                                case NONE -> "-";
-                                case FORWARD -> "F";
-                                case BACKWARD -> "B";
-                                }))
+                                .stateOverlay(BalanceMode.NONE, IKey.str("M:-"))
+                                .stateOverlay(BalanceMode.FORWARD, IKey.str("M:F"))
+                                .stateOverlay(BalanceMode.BACKWARD, IKey.str("M:B"))
                                 .addTooltipLine("Cycle Balance Modes"))
                         .child(
-                            new EnumCycleButtonWidget<>(SummaryMode.class)
+                            new CycleButtonWidget()
                                 .value(
                                     new EnumValue.Dynamic<>(
                                         SummaryMode.class,
@@ -194,7 +192,8 @@ public class FlowchartScreen extends ModularScreen {
                                             .getSummaryMode(),
                                         val -> Plan.getInstance()
                                             .setSummaryMode(val)))
-                                .overlay(val -> IKey.str("S:" + (val == SummaryMode.CYCLES ? "C" : "T")))
+                                .stateOverlay(SummaryMode.CYCLES, IKey.str("S:C"))
+                                .stateOverlay(SummaryMode.THROUGHPUT, IKey.str("S:T"))
                                 .addTooltipLine("Cycle Summary Modes"))
                         .child(new ButtonWidget<>().onMousePressed(_ -> {
                             canvas.addGroup(canvas.getCanvasScreenCenterX(), canvas.getCanvasScreenCenterY());
